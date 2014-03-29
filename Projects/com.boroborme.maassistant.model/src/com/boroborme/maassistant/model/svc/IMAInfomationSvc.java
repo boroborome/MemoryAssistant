50071df69dcc513c3e46f82f952512356f18546f
boroborome <boroborome@sina.com>
2014-03-29 22:14:49 +0800

    Define model and service interface

---
+++ Projects/com.boroborme.maassistant.model/src/com/boroborme/maassistant/model/svc/IMAInfomationSvc.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package com.boroborme.maassistant.model.svc;
+
+import com.boroborme.maassistant.model.MAInfomation;
+import com.boroborome.footstone.svc.IDataSvc;
+
+/**
+ * @author boroborome
+ *
+ */
+public interface IMAInfomationSvc extends IDataSvc<MAInfomation>
+{
+
+}
---
+++ Projects/com.boroborme.maassistant.model/src/com/boroborme/maassistant/model/svc/IMAKeywordSvc.java
@@ -0,0 +1,21 @@
+/*
+ * <P>Title:      任务事件管理器 V1.0</P>
+ * <P>Description:任务事件管理器接口</P>
+ * <P>Copyright:  Copyright (c) 2008</P>
+ * <P>Company:    BoRoBoRoMe Co. Ltd.</P>
+ * @author        BoRoBoRoMe
+ * @version       1.0 2011-6-26
+ */
+package com.boroborme.maassistant.model.svc;
+
+import com.boroborome.footstone.svc.IDataSvc;
+
+
+/**
+ * 任务管理器接口
+ * @author BoRoBoRoMe
+ * @param <MAKeyword>
+ */
+public interface IMAKeywordSvc<MAKeyword> extends IDataSvc<MAKeyword>
+{
+}
