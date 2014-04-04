9b2fe884663f0b6b75a34f2524b033ed5f5bc09a
boroborome <boroborome@sina.com>
2014-04-05 00:01:53 +0800

    add simple information manage panel

---
+++ Projects/com.boroborome.ma.view/src/com/boroborome/ma/view/InformationPanel.java
@@ -0,0 +1,38 @@
+/**
+ * 
+ */
+package com.boroborome.ma.view;
+
+import com.boroborme.ma.model.MAInformation;
+import com.boroborome.footstone.exception.InputException;
+import com.boroborome.footstone.ui.AbstractDataPanel;
+
+/**
+ * @author boroborome
+ *
+ */
+public class InformationPanel extends AbstractDataPanel<MAInformation>
+{
+
+	@Override
+	public void showData(MAInformation value)
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void collectData(MAInformation value)
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void verifyInput() throws InputException
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+}
