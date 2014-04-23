9cb1d5ec9287f786ff2754ad0914c5ae5367af8b
boroborome <boroborome@sina.com>
2014-04-23 22:28:27 +0800

    config ignore in git

--- Release/run.sh
+++ Release/run.sh
@@ -1,1 +1,1 @@
-java -Dorg.osgi.framework.system.packages.extra=special.parent.package -XstartOnFirstThread -Declipse.ignoreApp=true -Dosgi.noShutdown=true -Djava.library.path=./lib -jar -Dlog4j.configuration=conf/log4j.xml lib/org.eclipse.osgi_3.5.2.R35x_v20100126.jar -configuration conf -clean
+java -Declipse.ignoreApp=true -Dosgi.noShutdown=true -Djava.library.path=./lib -jar -Dlog4j.configuration=conf/log4j.xml lib/org.eclipse.osgi_3.5.2.R35x_v20100126.jar -configuration conf -clean
